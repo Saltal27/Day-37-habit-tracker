2104f8e91d93ba2735abf451cc7860b503545f21
Saltal27 <omarmobarak53@gmail.com>
2023-03-03 17:38:17 +0300

    final

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ GRAPH_ID = "pythontest"
 # }
 
 # user_account_response = requests.post(url=user_api_endpoint, json=user_request_body)
+# print(user_account_response)
 
 # ---------------------------- CREATING A NEW GRAPH ------------------------------- #
 # graph_api_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs"
@@ -29,18 +30,29 @@ GRAPH_ID = "pythontest"
 # }
 
 # graph_response = requests.post(url=graph_api_endpoint, json=graph_request_body, headers=HEADERS)
+# print(graph_response)
 
-# ---------------------------- CREATING / UPDATING A PIXEL ------------------------------- #
+# ---------------------------- CREATING A NEW PIXEL ------------------------------- #
 today = datetime.now()
 
-pixel_api_endpoint = f"https://pixe.la/v1/users/{USERNAME}/{GRAPH_ID}/pythontest"
-pixel_request_body = {
+new_pixel_api_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_ID}"
+new_pixel_request_body = {
     "date": today.strftime("%Y%m%d"),
     "quantity": "9",
 }
 
-# pixel_response = requests.post(url=pixel_api_endpoint, json=pixel_request_body, headers=HEADERS)
+# pixel_response = requests.post(url=new_pixel_api_endpoint, json=new_pixel_request_body, headers=HEADERS)
+# print(pixel_response.text)
 
-# ---------------------------- DELETING A PIXEL ------------------------------- #
-delete_pixel_api_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
-delete_pixel_response = requests.delete(url=delete_pixel_api_endpoint, headers=HEADERS)
+# ---------------------------- UPDATING / DELETING AN EXISTING PIXEL ------------------------------- #
+existing_pixel_api_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+existing_pixel_request_body = {
+    "quantity": "1",
+}
+
+# delete_pixel_response = requests.delete(url=existing_pixel_api_endpoint, headers=HEADERS)
+# print(delete_pixel_response.text)
+#
+# update_pixel_response = requests.put(url=existing_pixel_api_endpoint,
+#                                      json=existing_pixel_request_body, headers=HEADERS)
+# print(update_pixel_response.text)
